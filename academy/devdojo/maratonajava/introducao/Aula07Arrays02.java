eb0e9e930ae1e8a7b32ca4776050d486c078e72b
KaiqueBorgesdeOliveira <kaique.borges.1995@gmail.com>
2024-10-28 11:33:01 -0300

    feat: add new classes aulas 6 & 7

---
+++ academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    // byte, short, int, long, float, double 0
+    // char '\u0000' ''
+    // boolean false
+    // String null
+    public static void main(String[] args) {
+        String[] teste = new String[3];
+        System.out.println(teste[0]);
+        System.out.println(teste[1]);
+        System.out.println(teste[2]);
+    }
+}
